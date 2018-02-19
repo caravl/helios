8b249d65adac4ef477ca6a7e62c9c7cd172e31a5
Cara Lang <clang115@icloud.com>
2018-02-19 10:17:47 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,33 +2,23 @@ const express = require('express')
 const app = express()
 const path = require('path')
 
-// hook up our Node.js app to API.AI token
 const apiai = require('apiai')('5bb200822009472b812baeac80a4b837')
 
-// serve up html, css, images, js
 app.use(express.static(path.join(__dirname, '.', '/views')))
 app.use(express.static(path.join(__dirname, '.', 'public')))
 
-// our server
 const server = app.listen(5000)
 
-// sockets
 const io = require('socket.io')(server)
 
-// servers up our html when going to localhost:5000/
 app.get('/', (req, res) => {
   console.log('listening on port 5000')
   res.sendFile('index.html')
 })
 
-// --------- SOCKETS LISTENING --------- //
-
-// server side sockets to listen for and receive the emit with the speech recognition
 io.on('connection', function(socket) {
   console.log('io on connection')
   socket.on('chat message', (text) => {
-    // take the text emited with 'chat message'
-    // get a reply from API.AI
     console.log('socket', text)
     let apiaiReq = apiai.textRequest(text, {
       sessionId: 'APIAI_SESSION_ID'
@@ -37,7 +27,7 @@ io.on('connection', function(socket) {
     apiaiReq.on('response', (response) => {
 
       let aiText = response.result.fulfillment.speech
-      socket.emit('bot reply', aiText) // send result back to browser
+      socket.emit('bot reply', aiText)
       console.log('send reply back to browser')
     })
 
@@ -45,7 +35,7 @@ io.on('connection', function(socket) {
       console.log('error: ', error)
     })
 
-    apiaiReq.end() // keeps from responding to itself
+    apiaiReq.end()
 
   })
 
